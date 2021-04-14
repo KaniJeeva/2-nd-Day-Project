ccb955a999dc57f939def38ac2214e7fc369b93e
KaniJeeva <kani.ayyanar1993@gmail.com>
2021-04-14 12:00:30 +0530

    Today My Git Project

---
+++ src/test/java/org/sample/GitProjectClass.java
@@ -0,0 +1,18 @@
+package org.sample;
+
+public class GitProjectClass {
+	private void empId() {
+		
+		System.out.println("Employee Id is 1234");
+	}
+	private void empName() {
+		System.out.println("Employee name is kani");
+
+	}
+	public static void main(String[] args) {
+		GitProjectClass g= new GitProjectClass();
+		g.empId();
+		g.empName();
+	}
+
+}
